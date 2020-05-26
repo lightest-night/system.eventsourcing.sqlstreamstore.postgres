a756729b6cc13b68f269de41363edc05f5b7619a
James Woodley <james@lightestnight.com>
2020-05-26 16:08:04 +0100

    Use the INSERT INTO ON CONFLICT DO pattern for Upserting in PostgreSQL

--- Postgres/Scripts/SetCheckpoint.sql
+++ Postgres/Scripts/SetCheckpoint.sql
@@ -1,7 +1,4 @@
-﻿IF EXISTS ( SELECT checkpoint_id FROM __schema__.checkpoints WHERE checkpoint_id = @CheckpointId )
-    UPDATE __schema__.checkpoints
-    SET checkpoint = @Checkpoint
-    WHERE checkpoint_id = @CheckpointId;
-ELSE
-    INSERT INTO __schema__.checkpoints (checkpoint_id, checkpoint)
-    VALUES (@CheckpointId, @Checkpoint);
+﻿INSERT INTO __schema__.checkpoints (checkpoint_id, checkpoint)
+VALUES (@CheckpointId, @Checkpoint)
+ON CONFLICT (checkpoint_id)
+DO UPDATE SET checkpoint = @Checkpoint
